15ba6d6668cbb8fc5fe313ba416794c89c5dcfdc
Yaron <horsky.yaron@gmail.com>
2023-12-23 16:03:10 +0200

    feat(socket): added env variables

--- app/lib/socket.ts
+++ app/lib/socket.ts
@@ -1,6 +1,9 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:4000", {
-  transports: ["websocket"],
-  autoConnect: false,
-});
+export const socket = io(
+  `http://${process.env.NODE_SERVER_IP}:${process.env.NODE_SERVER_PORT}`,
+  {
+    transports: ["websocket"],
+    autoConnect: false,
+  },
+);
