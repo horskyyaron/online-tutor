8c9203114007dd83e4b45c867506b82b30b400b0
Yaron <horsky.yaron@gmail.com>
2023-12-24 13:53:37 +0200

    added prisma, created schama and seed file and command in package.json

---
+++ prisma/seed.ts
@@ -0,0 +1,34 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+async function main() {
+    await prisma.problem.createMany({
+        data: [
+            {
+                title: "Sum of Numbers",
+                starterCode: "function sumUpTo(n) {\n    let sum = 0;\n    // Your code here\n    return sum;\n}\n\nconsole.log(sumUpTo(5)); // Should return 15",
+            },
+            {
+                title: "String Reversal",
+                starterCode: "function reverseString(str) {\n    let reversed = '';\n    // Your code here\n    return reversed;\n}\n\nconsole.log(reverseString(\"hello\")); // Should return \"olleh\"",
+            },
+            {
+                title: "Find the Largest Number",
+                starterCode: "function findLargest(arr) {\n    let largest = arr[0];\n    // Your code here\n    return largest;\n}\n\nconsole.log(findLargest([3, 1, 4, 6, 2, 8])); // Should return 8",
+            },
+            {
+                title: "Checking for Palindromes",
+                starterCode: "function isPalindrome(str) {\n    // Your code here\n    return true; // or false\n}\n\nconsole.log(isPalindrome(\"racecar\")); // Should return true",
+            }
+        ],
+    });
+}
+
+main()
+    .catch((e) => {
+        console.error(e);
+        process.exit(1);
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
