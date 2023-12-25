7f68f1ee7f65d769c64a8579a525558136cbdb2a
Yaron <horsky.yaron@gmail.com>
2023-12-25 13:07:18 +0200

    changed https to wss

--- app/lib/socket.ts
+++ app/lib/socket.ts
@@ -4,12 +4,10 @@ const IP =
   process.env.NODE_ENV == "development"
     ? "localhost"
     : process.env.NEXT_PUBLIC_NODE_SERVER_IP;
-const PORT =
-  process.env.NODE_ENV == "development"
-    ? "4000"
-    : process.env.NEXT_PUBLIC_NODE_SERVER_IP;
+const PORT = process.env.NODE_ENV == "development" ? "4000" : "";
 
-const PROTOCOL = process.env.NODE_ENV === "development" ? "http" : "https"
+const PROTOCOL = process.env.NODE_ENV === "development" ? "http" : "wss";
+
 export const socket = io(`${PROTOCOL}://${IP}:${PORT}/`, {
   transports: ["websocket"],
   autoConnect: false,
