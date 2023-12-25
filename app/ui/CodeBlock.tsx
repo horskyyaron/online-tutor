ca2a422558c4e31ea9da78696942a012fc9d1961
Yaron <horsky.yaron@gmail.com>
2023-12-25 14:49:27 +0200

    some cleanup

--- app/ui/CodeBlock.tsx
+++ app/ui/CodeBlock.tsx
@@ -3,12 +3,8 @@
 import { useRef, useEffect, useState } from "react";
 import { socket } from "../lib/socket";
 import { HandshakeData, UpdatedTextData } from "../lib/defenitions";
-import hljs from "highlight.js/lib/core";
-import "highlight.js/styles/tokyo-night-dark.css";
-import javascript from "highlight.js/lib/languages/javascript";
 import { editor as MonacoEditor } from "monaco-editor";
 import { Editor } from "@monaco-editor/react";
-hljs.registerLanguage("javascript", javascript);
 
 export default function CodeBlock({ starterCode }: { starterCode: string }) {
   const [isConnected, setIsConnected] = useState(socket.connected);
@@ -21,49 +17,32 @@ export default function CodeBlock({ starterCode }: { starterCode: string }) {
     setIsConnected(true);
   }
 
-  function onDisconnect() {
-    console.log("disconnected!");
-    setIsConnected(false);
-  }
-
   function onHandshake(data: HandshakeData) {
-    if (role === "") {
-      setRole(data.role);
-    }
+    console.log("on handshake", data.role);
+    setRole(data.role);
   }
 
   function onTextChange(data: UpdatedTextData) {
-    console.log("updating only on tutor!");
     //no need to update state on student since he is the one writing in real time,
     //only the tutor state should change so he can see the changes in the text.
-    if (role == "tutor") setText(data.updatedText);
+    setText(data.updatedText);
   }
 
   // setting all the sockets events
   useEffect(() => {
-    socket.connect();
+    if (!socket.connected) {
+      socket.connect();
+    }
     socket.on("connect", onConnect);
-    socket.on("disconnect", onDisconnect);
     socket.on("handshake", onHandshake);
     socket.on("text change", onTextChange);
 
     // on unmount, close the socket.
     return () => {
-      socket.off("connect", onConnect);
-      socket.off("disconnect", onDisconnect);
       // TODO
       socket.disconnect();
     };
   }, []);
-
-  useEffect(() => {
-    const code = document.getElementById("codeblock");
-    if (code) {
-      // Unset the highlighted dataset to allow re-highlighting
-      code.removeAttribute("data-highlighted");
-      hljs.highlightElement(code);
-    }
-  }, [text]);
 
   // emit server that text has changed so it can brodcast it to the other clients (the tutor will be 'updated')
   function handleTextChange() {
@@ -80,6 +59,7 @@ export default function CodeBlock({ starterCode }: { starterCode: string }) {
         options={{ readOnly: role === "tutor" }}
         value={text}
         onChange={handleTextChange}
+        //getting the editor so we can extract its value and emit it to the tutor.
         onMount={(editor, monaco) => {
           editorRef.current = editor;
         }}
