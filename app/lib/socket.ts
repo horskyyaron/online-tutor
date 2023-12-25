117adbac4592e9fb28f312fdea901572a6c8ee86
Yaron <horsky.yaron@gmail.com>
2023-12-25 12:57:55 +0200

    feat(socket.ts): changing protocol based on env
    when in production, use https protocol instead of http in dev

--- app/lib/socket.ts
+++ app/lib/socket.ts
@@ -9,7 +9,8 @@ const PORT =
     ? "4000"
     : process.env.NEXT_PUBLIC_NODE_SERVER_IP;
 
-export const socket = io(`http://${IP}:${PORT}/`, {
+const PROTOCOL = process.env.NODE_ENV === "development" ? "http" : "https"
+export const socket = io(`${PROTOCOL}://${IP}:${PORT}/`, {
   transports: ["websocket"],
   autoConnect: false,
 });
