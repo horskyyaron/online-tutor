c4327968684983c0dc82a7636a560cfe41f2a588
Yaron <horsky.yaron@gmail.com>
2023-12-26 19:28:54 +0200

    fixed wrong port number

--- app/lobby/page.tsx
+++ app/lobby/page.tsx
@@ -3,36 +3,33 @@ import BlocksGallery from "./components/BlocksGallery";
 import prisma from "@/lib/db";
 
 export default async function Lobby() {
-    const protocol = process.env.NODE_ENV == "development" ? "http" : "https";
-    const ip =
-        process.env.NODE_ENV == "development"
-            ? "localhost"
-            : process.env.NEXT_PUBLIC_NODE_SERVER_IP;
-    const port =
-        process.env.NODE_ENV == "development"
-            ? "4000"
-            : process.env.NEXT_PUBLIC_NODE_SERVER_PORT;
+  const protocol = process.env.NODE_ENV == "development" ? "http" : "https";
+  const ip =
+    process.env.NODE_ENV == "development"
+      ? "localhost"
+      : process.env.NEXT_PUBLIC_NODE_SERVER_IP;
+  const port = process.env.NODE_ENV == "development" ? "4000" : "";
 
-    console.log(protocol, ip, port);
+  console.log(protocol, ip, port);
 
-    const serverStatus = await axios
-        .get(`${protocol}://${ip}:${port}/status`)
-        .then((res) => {
-            return res.data;
-        })
-        .catch((e) => {
-            console.log(e);
-        });
+  const serverStatus = await axios
+    .get(`${protocol}://${ip}:${port}/status`)
+    .then((res) => {
+      return res.data;
+    })
+    .catch((e) => {
+      console.log(e);
+    });
 
-    const challenges = await prisma.challenge.findMany();
+  const challenges = await prisma.challenge.findMany();
 
-    return (
-        <main>
-            <h1 className="mb-5">Coding Challenges</h1>
-            <BlocksGallery
-                serverStatus={serverStatus.session}
-                challenges={challenges}
-            />
-        </main>
-    );
+  return (
+    <main>
+      <h1 className="mb-5">Coding Challenges</h1>
+      <BlocksGallery
+        serverStatus={serverStatus.session}
+        challenges={challenges}
+      />
+    </main>
+  );
 }
